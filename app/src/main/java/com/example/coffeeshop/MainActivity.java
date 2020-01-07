72c4869aa6ad644d95e613509297f17222bb8176
ananthalakshmi-sat <anu.294@gmail.com>
2020-01-07 09:24:06 +0900

    Screenshots for readme

--- app/src/main/java/com/example/coffeeshop/MainActivity.java
+++ app/src/main/java/com/example/coffeeshop/MainActivity.java
@@ -56,9 +56,11 @@ public class MainActivity extends AppCompatActivity {
         //Set no navigation bar
         View overlay = findViewById(R.id.drawer_layout);
 
-        overlay.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            overlay.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
